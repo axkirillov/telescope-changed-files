5cc3d7745a34fef0e2365a52818a29bed2237942
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2022-05-20 15:33:09 +0200

    move command execution into func

--- lua/telescope/_extensions/changed-files.lua
+++ lua/telescope/_extensions/changed-files.lua
@@ -7,24 +7,26 @@ if not has_telescope then
   error('This plugins requires nvim-telescope/telescope.nvim')
 end
 
-local command = "git diff --name-only $(git merge-base HEAD develop)"
-local handle = io.popen(command)
-local result = handle:read("*a")
-handle:close()
-files = {}
-for token in string.gmatch(result, "[^%s]+") do
-   table.insert(files, token)
-end
-
 local changed_files = function(opts)
-  opts = opts or {}
-  pickers.new(opts, {
-    prompt_title = "colors",
-    finder = finders.new_table {
-      results = files
-    },
-    sorter = conf.generic_sorter(opts),
-  }):find()
+	local command = "git diff --name-only $(git merge-base HEAD develop)"
+	local handle = io.popen(command)
+	local result = handle:read("*a")
+	handle:close()
+
+	files = {}
+	for token in string.gmatch(result, "[^%s]+") do
+	   table.insert(files, token)
+	end
+
+	opts = opts or {}
+
+	pickers.new(opts, {
+		prompt_title = "colors",
+		finder = finders.new_table {
+			results = files
+		},
+		sorter = conf.generic_sorter(opts),
+	}):find()
 end
 
 return telescope.register_extension {
