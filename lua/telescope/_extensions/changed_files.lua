6fb252c0115d9d99ce1e87da69b109e1f09c1033
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2022-05-31 17:13:34 +0200

    add base branch picker

--- lua/telescope/_extensions/changed_files.lua
+++ lua/telescope/_extensions/changed_files.lua
@@ -2,6 +2,9 @@ local has_telescope, telescope = pcall(require, 'telescope')
 local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
 local conf = require("telescope.config").values
+local builtin = require("telescope.builtin")
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
 
 if not has_telescope then
   error('This plugin requires nvim-telescope/telescope.nvim')
@@ -10,7 +13,8 @@ end
 M = {}
 
 M.changed_files = function(opts)
-	local command = "git diff --name-only $(git merge-base HEAD develop)"
+	local base_branch = vim.g.telescope_changed_files_base_branch or "develop"
+	local command = "git diff --name-only $(git merge-base HEAD " .. base_branch .. " )"
 	local handle = io.popen(command)
 	local result = handle:read("*a")
 	handle:close()
@@ -31,8 +35,24 @@ M.changed_files = function(opts)
 	}):find()
 end
 
+M.choose_base_branch = function(opts)
+  opts = opts or {}
+  builtin.git_branches({
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+		vim.g.telescope_changed_files_base_branch = selection.value
+		M.base_branch = selection.value
+      end)
+      return true
+    end,
+  })
+end
+
 return telescope.register_extension {
 	exports = {
-		changed_files = M.changed_files
+		changed_files = M.changed_files,
+		choose_base_branch = M.choose_base_branch
 	},
 }
